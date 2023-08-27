e6ce4d35e1202d35ba7cab24916a512467810736
gdtdavies <gdtdavies@gmail.com>
2023-08-27 19:07:44 +0200

    swapped order of the hexes so that it draws rank by rank instead of file by file

--- Source/GUI.cpp
+++ Source/GUI.cpp
@@ -101,11 +101,6 @@ void GUI::drawKnight(float x, float y, Colour c) {
 void GUI::drawQueen(float x, float y, Colour c) {
 	double sz = (hexSize * 0.5) / (screenWidth < screenHeight ? screenWidth : screenHeight);
 	float ratio = 0.71;
-	//drawLine(x - sz / 2. * ratio, y + sz / 2. * ratio, x + sz / 2. * ratio, y - sz / 2. * ratio, c);
-	//drawLine(x - sz / 2. * ratio, y - sz / 2. * ratio, x + sz / 2. * ratio, y + sz / 2. * ratio, c);
-	//
-	//drawLine(x - sz / 2., y, x + sz / 2., y, c);
-	//drawLine(x, y - sz / 2., x, y + sz / 2., c);
 	drawRook(x, y, c);
 	drawBishop(x, y, c);
 }
@@ -122,45 +117,75 @@ void GUI::drawKing(float x, float y, Colour c) {
 void GUI::drawBoard() {
 	double sz = hexSize / (screenWidth < screenHeight ? screenWidth : screenHeight);
 
-	double x = -sz * 6;
-	double y = -sz / 2 - sz * 3;
+	//move the start point to the bottom left
+	double init_x = -sz * 5;
+	double init_y = -sz * 2.5;
+	
+	double x = init_x;
+	double y = init_y;
 
-	int ranksz = 5;
 	std::vector<int> dark = { 1,4,9,12,15,18,21,22,25,28,33,36,39,42,45,48,51,54,57,60,62,65,68,72,75,78,81,84,86,89 };
 	std::vector<int> med = { 2,5,7,10,13,16,19,23,26,29,31,34,37,40,43,46,49,52,55,58,61,63,66,69,73,76,79,82,85,87,90};
 	std::vector<int> light = { 3,6,8,11,14,17,20,24,27,30,32,35,38,41,44,47,50,53,56,59,64,67,70,71,74,77,80,83,88,91};
 
+	
+
+	//int ranksz = 5;
+	//int counter = 0;
+	//for (int file = 0; file < 11; file++) {
+	//	x += sz;
+	//	if (file < 6) {
+	//		y -= (sz / 2) * file;
+	//		ranksz++;
+	//	}
+	//	else {
+	//		y += (sz / 2) * (file - 10);
+	//		ranksz--;
+	//	}
+	//
+	//	for (int rank = 0; rank < ranksz; rank++) {
+	//		counter++;
+	//		y += sz ;
+	//
+	//		std::vector<float> colour;
+	//
+	//		if (std::count(dark.begin(), dark.end(), counter) != 0) 
+	//			colour = { 0.25, 0.2, 0.2 };
+	//		else if (std::count(med.begin(), med.end(), counter) != 0) 
+	//			colour = { 0.5, 0.45, 0.45 };
+	//		else if (std::count(light.begin(), light.end(), counter) != 0) 
+	//			colour = { 0.75, 0.7, 0.7 };
+	//		
+	//		drawHex(x, y, sz, colour);
+	//
+	//	}
+	//	y = -sz / 2 - sz * 3;
+	//
+	//}
+
+	int filesz = 6;
 	int counter = 0;
+	for (int rank = 0; rank < 11; rank++) {
+		rank < 6 ? x = init_x : x = init_x + sz * (rank - 5);
+		y = init_y + sz * rank;
+		if (rank > 5) y -= (sz / 2.) * (rank - 5);
 
-	for (int file = 0; file < 11; file++) {
-		x += sz;
-		if (file < 6) {
-			y -= (sz / 2) * file;
-			ranksz++;
-		}
-		else {
-			y += (sz / 2) * (file - 10);
-			ranksz--;
-		}
-
-		for (int rank = 0; rank < ranksz; rank++) {
+		for (int file = 0; file < filesz; file++) {
 			counter++;
-			y += sz ;
-
 			std::vector<float> colour;
-
-			if (std::count(dark.begin(), dark.end(), counter) != 0) 
+			if (std::count(dark.begin(), dark.end(), counter) != 0)
 				colour = { 0.25, 0.2, 0.2 };
-			else if (std::count(med.begin(), med.end(), counter) != 0) 
+			else if (std::count(med.begin(), med.end(), counter) != 0)
 				colour = { 0.5, 0.45, 0.45 };
-			else if (std::count(light.begin(), light.end(), counter) != 0) 
+			else if (std::count(light.begin(), light.end(), counter) != 0)
 				colour = { 0.75, 0.7, 0.7 };
-			
+
 			drawHex(x, y, sz, colour);
-
+			x += sz;
+			y -= sz / 2.;
+			
 		}
-		y = -sz / 2 - sz * 3;
-
+		rank < 5 ? filesz++ : filesz--;
 	}
 }
 
